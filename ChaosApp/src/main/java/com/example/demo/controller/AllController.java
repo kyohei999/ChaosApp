3a6bc35becbd8d39eb8aa69729aa3139a4d646ef
kyohei999 <66673838+kyohei999@users.noreply.github.com>
2021-04-30 21:28:26 +0900

    カオスプロジェクト始動

---
+++ ChaosApp/src/main/java/com/example/demo/controller/AllController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AllController {
+	@GetMapping("ChaosController")
+	public String findAll(Model model) {
+		String chaos = "やーーーーーーーーーはーーーー";
+		model.addAttribute("Chaos", chaos );
+		return "index";
+	}
+
+}
