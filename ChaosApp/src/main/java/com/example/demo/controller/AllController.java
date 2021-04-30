7ed728bee7db36c1c905142a9b2dfd41a888a132
kyohei999 <66673838+kyohei999@users.noreply.github.com>
2021-04-30 22:32:10 +0900

    ビュー追加

--- ChaosApp/src/main/java/com/example/demo/controller/AllController.java
+++ ChaosApp/src/main/java/com/example/demo/controller/AllController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AllController {
 	@GetMapping("ChaosController")
 	public String findAll(Model model) {
-		String chaos = "やーーーーーーーーーはーーーー";
+		String chaos = "デプロイ成功！！！";
 		model.addAttribute("Chaos", chaos );
-		return "index";
+		return "success";
 	}
 
 }
