fd0ecee2d2f7458c7ca81e2c3753508a1c721334
kyohei999 <66673838+kyohei999@users.noreply.github.com>
2021-04-30 23:35:46 +0900

    猫ちゃん画像追加

--- ChaosApp/src/main/java/com/example/demo/controller/AllController.java
+++ ChaosApp/src/main/java/com/example/demo/controller/AllController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AllController {
 	@GetMapping("ChaosController")
 	public String findAll(Model model) {
-		String chaos = "デプロイ成功！！！";
+		String chaos = "にゃおにゃお！！！";
 		model.addAttribute("Chaos", chaos );
 		return "success";
 	}
